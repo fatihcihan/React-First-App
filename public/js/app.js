d942a08db04bf1a58679ad2f93ae3b6dce05562d
Fatih <fatihcihan666@gmail.com>
2023-04-03 20:26:20 +0300

    app.js file created

---
+++ public/js/app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var template = /*#__PURE__*/React.createElement("h1", {
+  id: "header"
+}, "My first react app !"); // JSX -> Javascript XML
+var root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(template);
---
+++ src/app.js
@@ -0,0 +1,3 @@
+var template = <h1 id="header">My first react app !</h1>;  // JSX -> Javascript XML
+var root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(template);
