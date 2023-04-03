646b4b8f0aeb858324e9e6fefe3b6218241254d6
Fatih <fatihcihan666@gmail.com>
2023-04-03 20:50:35 +0300

    JSX syntax reviewed

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,18 @@
 "use strict";
 
+var productName = "Iphone 14";
+var productPrice = 40000;
+var product = {
+  name: "IPhone 15",
+  price: 55000
+};
+function formatPrice(product) {
+  return product.price + " TL";
+}
 var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", {
   id: "header"
-}, "My first react app !"), /*#__PURE__*/React.createElement("div", null, "Lorem ipsum dolor sit amet."), /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum.")));
+}, "Product List !"), /*#__PURE__*/React.createElement("div", {
+  "class": "product-details"
+}, /*#__PURE__*/React.createElement("h2", null, product.name), /*#__PURE__*/React.createElement("p", null, formatPrice(product)), /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."))));
 var root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(template);
--- src/app.js
+++ src/app.js
@@ -1,12 +1,27 @@
+var productName = "Iphone 14";
+var productPrice = 40000;
+
+var product = {
+    name: "IPhone 15",
+    price: 55000
+}
+
+function formatPrice(product) {
+    return product.price + " TL"
+}
+
 var template =
     <div>
-        <h1 id="header">My first react app !</h1>
-        <div>Lorem ipsum dolor sit amet.</div>
-        <ul>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-        </ul>
-    </div>;  
+        <h1 id="header">Product List !</h1>
+        <div class="product-details">
+            <h2>{product.name}</h2>
+            <p>{formatPrice(product)}</p>
+            <ul>
+                <li>Lorem, ipsum.</li>
+                <li>Lorem, ipsum.</li>
+                <li>Lorem, ipsum.</li>
+            </ul>
+        </div>
+    </div>;
 var root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(template);
