5d1b49eae4e55afe81515ccf008e9e5c2f82ecd9
Fatih <fatihcihan666@gmail.com>
2023-04-03 20:42:03 +0300

    Examined jsx syntax with the help of babel

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 "use strict";
 
-var template = /*#__PURE__*/React.createElement("h1", {
+var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", {
   id: "header"
-}, "My first react app !"); // JSX -> Javascript XML
+}, "My first react app !"), /*#__PURE__*/React.createElement("div", null, "Lorem ipsum dolor sit amet."), /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum.")));
 var root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(template);
--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
-var template = <h1 id="header">My first react app !</h1>;  // JSX -> Javascript XML
+var template =
+    <div>
+        <h1 id="header">My first react app !</h1>
+        <div>Lorem ipsum dolor sit amet.</div>
+        <ul>
+            <li>Lorem, ipsum.</li>
+            <li>Lorem, ipsum.</li>
+            <li>Lorem, ipsum.</li>
+        </ul>
+    </div>;  
 var root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(template);
