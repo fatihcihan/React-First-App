7679f3cd376dfad9e6f4f8a11902b6c2a14f84ce
Fatih <fatihcihan666@gmail.com>
2023-04-04 11:42:15 +0300

    products are listed with the map method

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,16 @@
 "use strict";
 
-var product = {
+var products = [{
   name: "IPhone 15",
-  price: 40000,
+  price: 60000,
   description: "nice phone",
   colors: ["Silver", "Red", "White"]
-};
+}, {
+  name: "IPhone 16",
+  price: 50000,
+  description: "nice phone",
+  colors: ["Black", "Red", "White"]
+}];
 function formatPrice(product) {
   return /*#__PURE__*/React.createElement("p", null, " ", product.price, " TL ");
 }
@@ -18,8 +23,15 @@ function formatPrice(product) {
 
 var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", {
   id: "header"
-}, "Product List !"), /*#__PURE__*/React.createElement("div", {
-  id: "product-details"
-}, product.name && product.name.length > 3 ? /*#__PURE__*/React.createElement("h2", null, " ", product.name, " ") : /*#__PURE__*/React.createElement("p", null, "product name not entered"), product.price && product.price > 0 && formatPrice(product), product.description && /*#__PURE__*/React.createElement("p", null, product.description), product.colors.length > 0 ? /*#__PURE__*/React.createElement("p", null, " color options available") : "", /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."))));
+}, "Product List "), products.map(function (product, index) {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "product-details",
+    key: index
+  }, product.name && product.name.length > 3 ? /*#__PURE__*/React.createElement("h2", null, " ", product.name, " ") : /*#__PURE__*/React.createElement("p", null, "product name not entered"), product.price && product.price > 0 && formatPrice(product), product.description && /*#__PURE__*/React.createElement("p", null, product.description), product.colors.length > 0 ? /*#__PURE__*/React.createElement("p", null, " color options available") : "", /*#__PURE__*/React.createElement("ul", null, product.colors.map(function (color, index) {
+    return /*#__PURE__*/React.createElement("li", {
+      key: index
+    }, " ", color);
+  })));
+}));
 var root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(template);
--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 
-var product = {
-    name: "IPhone 15",
-    price: 40000,
-    description: "nice phone",
-    colors: ["Silver", "Red", "White"]
-}
+var products = [
+    {
+        name: "IPhone 15",
+        price: 60000,
+        description: "nice phone",
+        colors: ["Silver", "Red", "White"]
+    },
+    {
+        name: "IPhone 16",
+        price: 50000,
+        description: "nice phone",
+        colors: ["Black", "Red", "White"]
+    }
+]
 
 function formatPrice(product) {
     return <p> {product.price} TL </p>
@@ -18,21 +26,24 @@ function formatPrice(product) {
 
 var template =
     <div>
-        <h1 id="header">Product List !</h1>
-        <div id="product-details">
-            {(product.name && product.name.length > 3) ? <h2> {product.name} </h2> : <p>product name not entered</p>}
-            {(product.price && product.price > 0) && formatPrice(product)}
-            {/* { printDescription(product) } */}
-            {product.description && <p>{product.description}</p>}
-
-            {product.colors.length > 0 ? <p> color options available</p> : ""}
-
-            <ul>
-                <li>Lorem, ipsum.</li>
-                <li>Lorem, ipsum.</li>
-                <li>Lorem, ipsum.</li>
-            </ul>
-        </div>
+        <h1 id="header">Product List </h1>
+        {
+            products.map((product, index) => (
+                <div className="product-details" key={index}>
+                    {(product.name && product.name.length > 3) ? <h2> {product.name} </h2> : <p>product name not entered</p>}
+                    {(product.price && product.price > 0) && formatPrice(product)}
+                    {/* { printDescription(product) } */}
+                    {product.description && <p>{product.description}</p>}
+                    {product.colors.length > 0 ? <p> color options available</p> : ""}
+                    <ul>
+                        {
+                            product.colors.map((color, index) => (<li key={index}> {color}</li>))
+                        }
+                    </ul>
+                </div>
+            ))
+        }
     </div >;
 var root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(template);
+
